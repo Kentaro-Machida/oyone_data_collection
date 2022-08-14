984549512f4283bfa6ca78d2eb07c5cff1c9472c
oyonecamera1 <kenkenm120319@gmail.com>
2022-08-14 19:07:56 +0900

    file to save jpg image

---
+++ frame_capture.py
@@ -0,0 +1,38 @@
+import cv2
+
+from prepare import prepare
+import os
+import time
+
+# WIDTH: MAX 2592, default 640
+# HEIGHT: MAX 1944, default 480
+# ただし、画素数を最大値にすると色チャネルがおかしくなる
+# (WIDTH, HEIGHT) = (1280, 960) までは正しく表示される
+
+WIDTH = 1440
+HEIGHT = 1240
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+frame_id = 0
+
+DIR_PATH = prepare(cap)
+
+start_time = time.perf_counter()
+while True:
+    try:
+        ret, frame = cap.read()
+        img_name = "frame_" + str(frame_id) + ".jpg"
+        img_path = str(os.path.join(DIR_PATH, img_name))
+        cv2.imwrite(img_path, frame)
+        frame_id += 1
+    except:
+        end_time = time.perf_counter()
+        execute_time = end_time - start_time
+        fps = int(frame_id / execute_time)
+        print(f"\n{fps=}")
+        print(f"frame size: {frame.shape}")
+        with open(os.path.join(DIR_PATH, "fps.txt"), "w") as f:
+            f.write(str(fps))
+            break
