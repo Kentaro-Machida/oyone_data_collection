a45ea06a3ca6041701a1c17ff4cd9a44ac82c7de
oyonecamera1 <kenkenm120319@gmail.com>
2022-08-15 18:06:26 +0900

    function to check cpu temperature

---
+++ check_cpu_temp.py
@@ -0,0 +1,17 @@
+import subprocess
+import re
+
+def get_cpu_temp()->str:
+    # 実行コマンド
+    cmd = "vcgencmd measure_temp"
+
+    # pythonからシェル呼び出し
+    temp = subprocess.check_output(cmd.split())
+
+    # 文字列として取得
+    temp = temp.decode("utf-8")
+    temp = re.sub('\n', '', temp)
+    return temp
+
+if __name__=='__main__':
+    print(get_cpu_temp())
