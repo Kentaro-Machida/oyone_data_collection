b09959b3f6e99b9826d780637451e079e10fab37
oyonecamera1 <kenkenm120319@gmail.com>
2022-08-15 18:06:57 +0900

    file to get video(mp4)

--- video_capture.py
+++ video_capture.py
@@ -1,1 +1,26 @@
 import cv2
+import numpy as np
+from prepare import prepare
+from check_cpu_temp import get_cpu_temp
+import os
+
+cap = cv2.VideoCapture(0)
+
+DIR_PATH = prepare(cap)
+
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+out = cv2.VideoWriter(os.path.join(DIR_PATH, "output.mp4"), fourcc, fps, (w, h))
+
+while True:
+    try:
+        ret, frame = cap.read()
+        out.write(frame)
+        print(get_cpu_temp())
+    except:
+        print(f"frame size: {frame.shape}")
+        break
+cap.release()
+out.release()
